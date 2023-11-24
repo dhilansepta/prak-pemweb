0345ea9f7537e171b7909aea15b9efddd7e42d12
Dhilan <103352717+dhilansepta@users.noreply.github.com>
2023-11-24 23:08:30 +0700

    prak 7

---
+++ prak7/index.php
@@ -0,0 +1,9 @@
+<!-- Dhilan Septa Yudha (120140224) PRAK7 PEMWEB RB  -->
+
+<?php
+    require 'datadiri.php';
+
+    $dataDiri = new DataDiri("Dhilan", 120140224, "Teknik Informatika", 2020);
+
+    echo $dataDiri->infoDataDiri() . "\n";
+?>
